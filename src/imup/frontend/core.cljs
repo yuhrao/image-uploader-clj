9a5b91218133fb9daf036229638babb742629267
Yuhri Graziano Bernardes <yuhri.graziano@gmail.com>
2022-12-28 07:57:31 -0300

    remove mocked data
    Signed-off-by: Yuhri Graziano Bernardes <yuhri.graziano@gmail.com>

--- src/imup/frontend/core.cljs
+++ src/imup/frontend/core.cljs
@@ -9,17 +9,6 @@
 (defonce dz (r.core/atom nil))
 (defonce uploaded-images (r.core/atom []))
 
-(reset! uploaded-images
-          [{:id "b84c4d00-f6a2-438f-8c81-acdf122197d8",
-            :path "/assets/b84c4d00-f6a2-438f-8c81-acdf122197d8-profile.jpg",
-            :name "profile.jpg",
-            :type "image/jpeg"}
-           {:id "d77d8b44-005e-4dc1-91e6-8bfb81c871a0",
-            :path "/assets/d77d8b44-005e-4dc1-91e6-8bfb81c871a0-this-is-fine-zoom-background.jpg",
-            :name "this-is-fine-zoom-background.jpg",
-            :type "image/jpeg"}]
-)
-
 (defn on-upload-finish [result]
   (swap! uploaded-images conj result))
 
